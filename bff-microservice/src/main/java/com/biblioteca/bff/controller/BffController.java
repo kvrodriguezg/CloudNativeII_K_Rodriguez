d26cae2edbf04220294a047cf3d687482b067ede
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2026-03-29 19:41:07 -0300

    azure

--- bff-microservice/src/main/java/com/biblioteca/bff/controller/BffController.java
+++ bff-microservice/src/main/java/com/biblioteca/bff/controller/BffController.java
@@ -38,8 +38,6 @@ public class BffController {
         return restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
     }
 
-    // --- NUEVO: Endpoints de Prestamos ---
-
     @GetMapping(value = "/prestamos", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getPrestamos() {
         String url = azureFunctionsBaseUrl + "/api/PrestamosFunction";
